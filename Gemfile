547e7eef8c52f9aa229ceab2ebebc15944c490b5
levisn1 <djlevisn1@gmail.com>
2018-09-22 15:13:53 +0200

    Ops lavorato sul master

--- Gemfile
+++ Gemfile
@@ -16,9 +16,8 @@ gem 'uglifier', '>= 1.3.0'
 # gem 'mini_racer', platforms: :ruby
 gem 'autoprefixer-rails'
 gem 'bootstrap-sass'
-gem 'bootstrap'
-gem 'font-awesome-sass', '~> 4.7'
 gem 'sass-rails'
+gem 'font-awesome-sass', '~> 4.7'
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.2'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_21_080211) do
+ActiveRecord::Schema.define(version: 2018_09_21_213656) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 2018_09_21_080211) do
     t.datetime "updated_at", null: false
     t.boolean "admin", default: false
     t.string "avatar_url", default: "https://www.w3schools.com/w3images/avatar2.png"
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
