5ae3e4a87dedfa4d36c88bc0ccf343f09079ad2e
levisn1 <djlevisn1@gmail.com>
2018-10-25 17:20:03 +0200

    50% completed index orders

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -2,10 +2,10 @@ module ProductsHelper
 
   def index_methods_partial
     if params[:term].present?
-      @products = Product.where(:ordinable => true).order('created_at DESC').search_by_name_and_description(params[:term]).paginate page: params[:page], per_page: 7
+      @products = Product.where(:ordinable => true).order('created_at DESC').search_by_name_and_description(params[:term]).paginate page: params[:page], per_page: 6
       "products/index_partials/search_results_products"
     else
-      @products = Product.where(:ordinable => true).order('created_at DESC').paginate page: params[:page], per_page: 7
+      @products = Product.where(:ordinable => true).order('created_at DESC').paginate page: params[:page], per_page: 27
       "products/index_partials/all_products"
     end
   end
