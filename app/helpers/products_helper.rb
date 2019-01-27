a094b2ee6cb18b0fc6b71618c468d0410d8abdd7
Luis Carlos Quarta <djlevisn1@gmail.com>
2019-01-27 22:55:04 +0100

    Changed showed products numbers in inpaginate settings

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -5,7 +5,7 @@ module ProductsHelper
       @products = Product.where(:ordinable => true).order('created_at DESC').search_by_name_and_description(params[:term]).paginate page: params[:page], per_page: 6
       "products/index_partials/search_results_products"
     else
-      @products = Product.where(:ordinable => true).order('created_at DESC').paginate page: params[:page], per_page: 27
+      @products = Product.where(:ordinable => true).order('created_at DESC').paginate page: params[:page], per_page: 12
       "products/index_partials/all_products"
     end
   end
